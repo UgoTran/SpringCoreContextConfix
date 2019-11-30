004cd77c11c2a78c0de40b1dde5aa50c97d71fae
UgoTran <bsk.denimit@gmail.com>
2019-11-30 18:43:51 +0700

    re-struct project

--- src/main/java/com/hivetech/springcore/xml/bean/A.java
+++
@@ -1,7 +0,0 @@
-package com.hivetech.springcore.xml.bean;
-
-public class A {
-    public A() {
-        System.out.println("beanA is created " + hashCode());
-    }
-}
--- src/main/java/com/hivetech/springcore/xml/bean/B.java
+++
@@ -1,7 +0,0 @@
-package com.hivetech.springcore.xml.bean;
-
-public class B {
-    public B() {
-        System.out.println("beanB is created " + hashCode());
-    }
-}
